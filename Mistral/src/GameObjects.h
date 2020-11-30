55b3e9579d67ae34d3e783c88626ba5a5e806937
unknown <vlnmnkv@gmail.com>
2020-12-01 01:22:25 +0900

    added a couple of bee enemies

--- Mistral/src/GameObjects.h
+++ Mistral/src/GameObjects.h
@@ -42,9 +42,9 @@ public:
 
 	int bodyCol[3] = { 255, 150, 0 };
 	int white[3] = { 255, 255, 255 };
-	int black[3] = { 0, 0, 0 };
-	int clothingCol[3] = { 255, 82, 0 };
-	int clothingCol2[3] = { 160, 82, 45 };
+	int black[3] = { 88, 54, 34 };
+	int clothingCol[3] = { 255, 92, 0 };
+	int clothingCol2[3] = { 255, 180, 10 };
 
 	float animation = 0;
 };
@@ -282,13 +282,166 @@ public:
 
 	void Update();
 
-	void DrawSelf() {
+	/* void DrawSelf() {
 		PUSH
 			COLOR3(200, 100, 0)
 			SCALE(0.7, 0.7, 0.5)
 			TRANSLATE(0, 0.5, 0)
 			CUBE
-		POP
+		POP */
+
+//////////// BIG BEE 
+	void DrawSelf() { 
+	// BEE BODY
+	PUSH
+		COLOR3(255, 238, 0);
+		TRANSLATE(0, 1.6, 0);
+		SCALE(0.8, 0.6, 0.2);
+		CUBE
+	POP
+	// BEE HEAD
+	PUSH
+		COLOR3(35, 26, 21);
+		TRANSLATE(-0.5, 1.6, 0);
+		SCALE(0.2, 0.5, 0.2);
+		CUBE
+	POP
+	// WING EYE
+	PUSH
+		COLOR3(255, 255, 255);
+		TRANSLATE(-0.5, 1.58, 0.09);
+		SCALE(0.1, 0.15, 0.03);
+		CUBE
+	POP
+	// BEE LINE
+	PUSH
+		COLOR3(45, 36, 31);
+		TRANSLATE(0, 1.6, 0);
+		SCALE(0.3, 0.7, 0.21);
+		CUBE
+	POP
+	// BEE B
+	PUSH
+		COLOR3(45, 36, 31);
+		TRANSLATE(0.45, 1.6, 0);
+		SCALE(0.12, 0.4, 0.15);
+		CUBE
+	POP
+	PUSH
+		COLOR3(45, 36, 31);
+		TRANSLATE(0.5, 1.6, 0);
+		SCALE(0.2, 0.1, 0.03);
+		CUBE
+	POP
+	PUSH
+		COLOR3(255, 140, 26);
+		TRANSLATE(0.63, 1.6, 0);
+		SCALE(0.1, 0.1, 0.03);
+		CUBE
+	POP
+
+	// PAWS
+	PUSH
+		COLOR3(45, 36, 31);
+		TRANSLATE(-0.3, 1.25, -0.06);
+		SCALE(0.1, 0.25, 0.04);
+		CUBE
+	POP
+	PUSH
+		COLOR3(35, 26, 21);
+		TRANSLATE(-0.25, 1.25, 0.06);
+		SCALE(0.1, 0.25, 0.04);
+		CUBE
+	POP
+	// WING LEFT
+	PUSH
+		COLOR3(128, 255, 255); // BLUE
+		TRANSLATE(-0.15, 2, 0.03);
+		SCALE(0.2, 0.5, 0.12);
+		CUBE
+	POP
+	// WING RIGHT
+	PUSH
+		COLOR3(128, 255, 255);
+		TRANSLATE(0.13, 1.9, -0.03);
+		SCALE(0.2, 0.5, 0.12);
+		CUBE
+	POP		
+////////////// SMALL BEE 
+	// BEE BODY 
+	PUSH
+		COLOR3(255, 238, 0);
+		TRANSLATE(2, 0.7, 0);
+		SCALE(0.4, 0.3, 0.1);
+		CUBE
+	POP
+	// BEE HEAD
+	PUSH
+		COLOR3(35, 26, 21);
+		TRANSLATE(1.745, 0.7, 0);
+		SCALE(0.1, 0.25, 0.1);
+		CUBE
+	POP
+	// WING EYE
+	PUSH
+		COLOR3(255, 255, 255);
+		TRANSLATE(1.745, 0.654, 0.045);
+		SCALE(0.05, 0.075, 0.015);
+		CUBE
+	POP
+	// BEE LINE
+	PUSH
+		COLOR3(45, 36, 31);
+		TRANSLATE(2, 0.7, 0);
+		SCALE(0.18, 0.37, 0.11);
+		CUBE
+			POP
+	// BEE B
+	PUSH
+		COLOR3(45, 36, 31);
+		TRANSLATE(2.24, 0.7, 0);
+		SCALE(0.06, 0.2, 0.075);
+		CUBE
+	POP
+	PUSH
+		COLOR3(45, 36, 31);
+		TRANSLATE(2.28, 0.7, 0);
+		SCALE(0.1, 0.05, 0.05);
+		CUBE
+	POP
+	PUSH
+		COLOR3(115, 230, 0);
+		TRANSLATE(2.35, 0.7, 0);
+		SCALE(0.04, 0.05, 0.05);
+		CUBE
+	POP
+	// PAWS
+	PUSH
+		COLOR3(45, 36, 31);
+		TRANSLATE(1.87, 0.525, -0.03);
+		SCALE(0.06, 0.125, 0.02);
+		CUBE
+	POP
+	PUSH
+		COLOR3(35, 26, 21);
+		TRANSLATE(1.85, 0.52, 0.03);
+		SCALE(0.06, 0.125, 0.02);
+		CUBE
+	POP
+	// WING LEFT
+	PUSH
+		COLOR3(128, 255, 255); // BLUE
+		TRANSLATE(1.9, 0.85, -0.02);
+		SCALE(0.1, 0.25, 0.05);
+		CUBE
+	POP
+	// WING RIGHT
+	PUSH
+		COLOR3(128, 255, 255);
+		TRANSLATE(2.05, 0.88, 0.02);
+		SCALE(0.1, 0.25, 0.05);
+		CUBE
+	POP
 	}
 
 	float vspd = 0;
